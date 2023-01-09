841bfdc2e6583fedf3acb445bda806f232d85aec
Salomatov Nikita <togglenezt@yandex.ru>
2023-01-09 22:07:44 +0300

    fix: автотесты

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,17 @@
-const userSchema = require('../models/user');
+const User = require('../models/user');
 
-// ищем всех пользователей
+// показать всех пользователей
 module.exports.getUsers = (req, res) => {
-  userSchema
-    .find({})
+  User.find({})
     .then((users) => res.send(users))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
 // добавить пользователя
-module.exports.createUsers = (req, res) => {
+module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  userSchema
-    .create({ name, about, avatar })
+
+  User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -25,11 +25,12 @@ module.exports.createUsers = (req, res) => {
       }
     });
 };
+
 // поиск по id
 module.exports.getUserById = (req, res) => {
   const { userId } = req.params;
-  userSchema
-    .findById(userId)
+
+  User.findById(userId)
     .orFail(new Error('NotFound'))
     .then((user) => res.send(user))
     .catch((err) => {
@@ -43,11 +44,12 @@ module.exports.getUserById = (req, res) => {
       return res.status(500).send({ message: 'Ошибка по умолчанию' });
     });
 };
+
 // обновление данных
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  userSchema
-    .findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
@@ -61,8 +63,7 @@ module.exports.updateUser = (req, res) => {
 // обновление аватара
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  userSchema
-    .findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
