9973e49c64e48e84f2c40b5e351cd9d132d4a7f8
Salomatov Nikita <togglenezt@yandex.ru>
2023-01-30 18:14:57 +0300

    fix: Автотесты

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const AuthErr = require('../errors/AuthErr');
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
